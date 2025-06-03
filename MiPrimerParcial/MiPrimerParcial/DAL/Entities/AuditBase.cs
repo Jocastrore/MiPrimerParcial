08237036dacd6d200cfa208f7dba9e8c1e894fcc
Jocastrore <alejitoking123@gmail.com>
2025-06-03 09:51:51 -0500

    1 parte del segundo entregable

--- MiPrimerParcial/MiPrimerParcial/DAL/Entities/AuditBase.cs
+++ MiPrimerParcial/MiPrimerParcial/DAL/Entities/AuditBase.cs
@@ -5,10 +5,12 @@ namespace MiPrimerParcial.DAL.Entities
     public class AuditBase
     {
         [Key]
-        [Required]
+
         public virtual Guid Id { get; set; }
-        public virtual DateTime? CreatedDate { get; set; }
-        public virtual DateTime? ModifiedDate { get; set; }
+
+        public virtual DateTime CreatedDate { get; set; }
+
+        public virtual DateTime ModifiedDate { get; set; }
 
     }
 }
--- MiPrimerParcial/MiPrimerParcial/DAL/Entities/Country.cs
+++ MiPrimerParcial/MiPrimerParcial/DAL/Entities/Country.cs
@@ -4,9 +4,9 @@ namespace MiPrimerParcial.DAL.Entities
 {
     public class Country : AuditBase
     {
-        [Required]
-        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo de {1} caracter")]
         [Display(Name = "Pais")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo de {1} caracter")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string Name { get; set; }
     }
 }

e90899c5aeeea8cfd5099b4aa59cffe1c9e59819
Jocastrore <alejitoking123@gmail.com>
2025-06-03 23:45:13 -0500

    Seeder created

---
+++ MiPrimerParcial/MiPrimerParcial/DAL/SeederDB.cs
@@ -0,0 +1,60 @@
+﻿namespace MiPrimerParcial.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBaseContext _context;
+
+        public SeederDB(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            await PopulateCountriesAsync();
+
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task PopulateCountriesAsync()
+        {
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Colombia"
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Antioquia"
+                        },
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Cundinamarca"
+                        }
+                    }
+                });
+
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Argentina",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Buenos Aires"
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
--- MiPrimerParcial/MiPrimerParcial/Program.cs
+++ MiPrimerParcial/MiPrimerParcial/Program.cs
@@ -11,14 +11,27 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ICountryService, CountryService>();
-
+builder.Services.AddScoped<IStateService, StateService>();
+builder.Services.AddTransient<SeederDB>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+SeederData();
+
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory?.CreateScope())
+    {
+        SeederDB? service = scope.ServiceProvider.GetService<SeederDB>();
+        service.SeederAsync().Wait();
+    }
+}
+    // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
